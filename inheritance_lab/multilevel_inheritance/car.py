df7e3a21405de3028020ee52f8e40c6b7bb0b76a
Svetlio-T <152528140+Svetlio-T@users.noreply.github.com>
2023-12-06 21:33:38 +0000

    Added more solutions

---
+++ inheritance_lab/multilevel_inheritance/car.py
@@ -0,0 +1,6 @@
+from inheritance_lab.multilevel_inheritance.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return "driving..."
---
+++ inheritance_lab/multilevel_inheritance/sports_car.py
@@ -0,0 +1,6 @@
+from inheritance_lab.multilevel_inheritance.car import Car
+
+
+class SportsCar(Car):
+    def race(self):
+        return "racing..."
